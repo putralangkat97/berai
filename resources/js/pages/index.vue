afe1bcabcdce4d388c1be76597b7e11dd11c783e
Anggit Utomo <pucukpisang97@gmail.com>
2025-07-11 22:00:54 +0700

    feat: :rocket: init project

---
+++ resources/js/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Guest from "../layouts/guest.vue";
+
+defineOptions({ layout: Guest });
+</script>
+
+<template>
+    <div class="py-4">
+        <button class="bg-amber-500 px-4 py-3">Hello</button>
+    </div>
+</template>
