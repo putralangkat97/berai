db21529484ec88dffc5e7dedc46daad0b611bf91
Anggit Utomo <pucukpisang97@gmail.com>
2025-07-20 01:29:41 +0700

    feat: :sparkles: v0.10.1 enable prefetch link

---
+++ resources/js/components/layouts/app-link.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+  href: {
+    type: String,
+    default: "#",
+  },
+  prefetch: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
+
+<template>
+  <Link :href="href" :prefetch="prefetch">
+    <slot />
+  </Link>
+</template>
